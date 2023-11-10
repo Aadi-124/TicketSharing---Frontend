cd5de0f4596175acc4ff88b44f1750c10753a924
Aaditya Thakare <aadityathakare124@gmail.com>
2023-11-10 20:48:02 +0530

    Initial Commit!

--- lib/Page1.dart
+++ lib/Page1.dart
@@ -15,7 +15,7 @@ class Page1 extends StatelessWidget{
         title:const Text('Login Page'),
       ),
       body:const Center(
-        child:Text("This is For Gauri")
+        child:Text("This is for main")
       ),
       );
 
