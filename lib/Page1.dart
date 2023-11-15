3c631b634794354e2c0834d81ebc9b6bb279cd7d
Aaditya Thakare <aadityathakare124@gmail.com>
2023-11-15 17:16:03 +0530

    Update Page1.dart (#6)

--- lib/Page1.dart
+++ lib/Page1.dart
@@ -14,18 +14,12 @@ class Page1 extends StatelessWidget{
       appBar:AppBar(
         title:const Text('Login Page'),
       ),
-<<<<<<< HEAD
+
       body:const Center(
         child:Text("This is for main")
       ),
-=======
       body: Column(
 	child:Text("This is Edited for Seetigns!")
-<<<<<<< HEAD
->>>>>>> Gauri
-=======
-	      "EDITED BY GAURI!"
->>>>>>> Gauri
       );
 
   }
